7123aaa72801f60155a6ad714e241812283038ca
timothyrusso <russotimothy@live.it>
2022-01-26 08:14:25 +0100

    feat: reviews component code refactoring

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Reviews.css';
 
-function Reviews (props) {
 
+function Reviews(props) {
   return (
-    // you'll need to update the code inside the map function
     <>
       <ul className="reviews">
-        {props.reviews && props.reviews.map((review)=>{
+        {props.reviews && props.reviews.map((review) => {
           return <li key={review.id} className="reviews__item"><Link to={`/reviews/${review.id}`}>{review.title}</Link></li>
         })}
       </ul>
